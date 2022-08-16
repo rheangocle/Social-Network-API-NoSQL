1321dcbc985add4477980ff3dbee26a388a2d1ae
rheangocle <rheangocle@gmail.com>
2022-08-15 23:48:21 -0400

    adding seeds for thoughts and reactions

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,10 +36,9 @@ module.exports = {
   getSingleUser(req, res) {
     //this could also be User.findById
     User.findOne({ _id: req.params.userId })
-      .select('-__v')
-      //populating thoughts and friends
       .populate('thoughts')
       .populate('friends')
+      .select('-__v')
       .then((user) =>
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
